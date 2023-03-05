a4029f674c9887a715a7b89e2cba5e6a07bd35a0
AmrMMohamed <AmrMMohamed@github.com>
2023-03-05 06:59:02 +0200

    changing the code from c++

--- c++/main.cpp
+++ c++/main.cpp
@@ -6,4 +6,11 @@ int main()
     cout<<"hi amr mohamed";
     cout<<"my name is mahmoud";
     cout<<"enter the number plzz";
+    int x;
+    cin>>x;
+    for (size_t i = 0; i < x; i++)
+    {
+        cout<<"hi amr moahmed"<<endl;
+    }
+    
 }
